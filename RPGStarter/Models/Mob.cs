164df937e651e814a779015114c3050a256bd845
[daveclingen] <[opphyster31@hotmail.com]>
2018-05-22 11:21:39 -0700

    issue #5
    I added model class mob.cs.

---
+++ RPGStarter/Models/Mob.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RPGStarter.Models
+{
+    public class Mob
+    {
+        [Required]
+        public int mob_id { get; set; }
+
+        [Required]
+        public string mob_name { get; set; }
+
+        [Required]
+        public int item_id { get; set; }
+
+        [Required]
+        public string item_name { get; set; }
+
+        [Required]
+        public int stat_id { get; set; }
+
+        [Required]
+        public Boolean is_hostile { get; set; }
+    }
+}
