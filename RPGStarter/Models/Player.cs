b1229c92e08a152ec4b001eac12d82482f90575a
Shane Butcher <sgbutcher@outlook.com>
2018-05-21 21:51:59 -0700

    Issue: Create player model #3 Added Player, PlayerClass, and PlayerInventory Models

---
+++ RPGStarter/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RPGStarter.Models
+{
+    public class Player
+    {
+        public int PlayerID { get; set; }
+        public string OwnerID { get; set; }
+        public string Name { get; set; }
+        public int StatID { get; set; }
+        public Stat Stat { get; set; }
+    }
+}
---
+++ RPGStarter/Models/PlayerInventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RPGStarter.Models
+{
+    public class PlayerInventory
+    {
+        public int PlayerID { get; set; }
+        public int ItemID { get; set; }
+        public Player Player { get; set; }
+        public Item Item { get; set; }
+    }
+}
