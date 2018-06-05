c8f5f2341c2098036e3a882056c2a429e27801f2
A.C. Miller <acmiller03@gmail.com>
2018-06-05 12:19:31 -0700

    Items Controller
    Added items controller

---
+++ RPGStarter/Controllers/ItemsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RPGStarter.Data;
+using RPGStarter.Models;
+
+namespace RPGStarter.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Items
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Items.ToListAsync());
+        }
+
+        // GET: Items/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var items = await _context.Items
+                .SingleOrDefaultAsync(m => m.itemID == id);
+            if (items == null)
+            {
+                return NotFound();
+            }
+
+            return View(items);
+        }
+
+        // GET: Items/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Items/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("itemID,name,value,weight,damage,armorValue")] Items items)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(items);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(items);
+        }
+
+        // GET: Items/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var items = await _context.Items.SingleOrDefaultAsync(m => m.itemID == id);
+            if (items == null)
+            {
+                return NotFound();
+            }
+            return View(items);
+        }
+
+        // POST: Items/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("itemID,name,value,weight,damage,armorValue")] Items items)
+        {
+            if (id != items.itemID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(items);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ItemsExists(items.itemID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(items);
+        }
+
+        // GET: Items/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var items = await _context.Items
+                .SingleOrDefaultAsync(m => m.itemID == id);
+            if (items == null)
+            {
+                return NotFound();
+            }
+
+            return View(items);
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var items = await _context.Items.SingleOrDefaultAsync(m => m.itemID == id);
+            _context.Items.Remove(items);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ItemsExists(int id)
+        {
+            return _context.Items.Any(e => e.itemID == id);
+        }
+    }
+}
